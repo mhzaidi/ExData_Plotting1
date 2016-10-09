8979478f5ec6c3274b9f03e6e500cb15124d3cd3
mhzaidi <mhzaidi110@gmail.com>
2016-10-09 15:57:51 -0400

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,7 @@
+plot(data2$DateTime, as.numeric(as.character(data2$Sub_metering_1)),type='l', 
+     ylab ="Energy sub metering", xlab="")
+ lines(data2$DateTime, as.numeric(as.character(data2$Sub_metering_2)),type='l', col='red')
+ lines(data2$DateTime, data2$Sub_metering_3,type='l', col="blue")
+  legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                    lty=c(1,1,1)
+                    ,col=c("black","red","blue"))
